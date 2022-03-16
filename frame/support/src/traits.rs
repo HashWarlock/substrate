5ef78e38c47530b7234349d310fea8da1bb720f0
hashwarlock <hashwarlock@protonmail.com>
2022-03-16 14:42:43 -0500

    Expose Locker to tokens

--- frame/support/src/traits.rs
+++ frame/support/src/traits.rs
@@ -54,7 +54,7 @@ pub use misc::{
 	Backing, ConstBool, ConstI128, ConstI16, ConstI32, ConstI64, ConstI8, ConstU128, ConstU16,
 	ConstU32, ConstU64, ConstU8, DefensiveSaturating, EnsureInherentsAreFirst, EqualPrivilegeOnly,
 	EstimateCallFee, ExecuteBlock, ExtrinsicCall, Get, GetBacking, GetDefault, HandleLifetime,
-	IsSubType, IsType, Len, OffchainWorker, OnKilledAccount, OnNewAccount, PreimageProvider,
+	IsSubType, IsType, Len, Locker, OffchainWorker, OnKilledAccount, OnNewAccount, PreimageProvider,
 	PreimageRecipient, PrivilegeCmp, SameOrOther, Time, TryCollect, TryDrop, UnixTime,
 	WrapperKeepOpaque, WrapperOpaque,
 };
@@ -79,9 +79,6 @@ pub use hooks::{
 };
 #[cfg(feature = "try-runtime")]
 pub use hooks::{OnRuntimeUpgradeHelpersExt, ON_RUNTIME_UPGRADE_PREFIX};
-
-mod locker;
-pub use locker::Locker;
 
 pub mod schedule;
 mod storage;
