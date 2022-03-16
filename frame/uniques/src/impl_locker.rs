5021da82bdb90e575de6b41d2d0cda1614229816
hashwarlock <hashwarlock@protonmail.com>
2022-03-16 11:54:19 -0500

    Move impl () to misc

--- frame/uniques/src/impl_locker.rs
+++ frame/uniques/src/impl_locker.rs
@@ -17,13 +17,13 @@
 
 use frame_support::traits::Locker;
 
-impl<ClassId, InstanceId> Locker<ClassId, InstanceId> for () {
+impl<T: Config<I>, I: 'static> Locker<T::ClassId, T::InstanceId> for Pallet<T, I> {
     /// Check if the asset should be locked and prevent interactions with the asset from executing.
     /// Default will be false if not implemented downstream
     ///
     /// Note: The logic check in this function must be constant time and consistent for benchmarks
     /// to work
-    fn is_locked(_class: ClassId, _instance: InstanceId) -> bool {
+    fn is_locked(_class: T::ClassId, _instance: T::InstanceId) -> bool {
         false
     }
 }
